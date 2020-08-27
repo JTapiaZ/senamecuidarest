84cc4c4574c3ede566e4f8891b99984870811a81
jtapiaz <juanjotapias0414@gmail.com>
2020-08-27 15:08:41 -0500

    Bloqueo y desbloqueo medico / Bloqueo entrada / API - Bloqueo

--- src/Controllers/aprendizController.js
+++ src/Controllers/aprendizController.js
@@ -5,7 +5,7 @@ const {emailEnfermeroSendNE} = require('./mailRegistroNEController');
 
 exports.aprendiz_create = function (req, res) {
     // ------------------ Validate Request ----------------- //
-    if (!req.body.nombre || !req.body.email || !req.body.documentoIdentidad || !req.body.telefono || !req.body.direccionResidencia || !req.body.eps || !req.body.complejo) {
+    if (!req.body.nombre || !req.body.email || !req.body.documentoIdentidad || !req.body.telefono || !req.body.direccionResidencia || !req.body.eps ) {
         return res.status(400).send("¡Por favor rellene todos los campos solicitados!");
     }
 
@@ -72,7 +72,7 @@ exports.aprendiz_create = function (req, res) {
 
 exports.aprendiz_createNE = function (req, res) {
     // ------------------ Validate Request ----------------- //
-    if (!req.body.nombre || !req.body.email || !req.body.documentoIdentidad || !req.body.telefono || !req.body.direccionResidencia || !req.body.eps || !req.body.complejo) {
+    if (!req.body.nombre || !req.body.email || !req.body.documentoIdentidad || !req.body.telefono || !req.body.direccionResidencia || !req.body.eps ) {
         return res.status(400).send("¡Por favor rellene todos los campos solicitados!");
     }
 
